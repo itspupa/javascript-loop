59a67d60c6d75a9f7b188de26450cb73a3909558
Anuna <anuaq20@gmail.com>
2025-07-21 13:00:05 +0700

    refactor: rename variables for consistency in ex-2.js

--- ex-2.js
+++ ex-2.js
@@ -1,9 +1,9 @@
 // Exercise #2: Sum the Numbers
-let productPrices = [10, 20, 30, 40, 50];
+let prices = [10, 20, 30, 40, 50];
 let total = 0;
 // Start coding here
-for (let prices of productPrices){
-    total += prices;
+for (let productPrices of prices){
+    total += productPrices;
 }
 
 console.log(total);
